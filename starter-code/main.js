edeca9ceb5dc0a81f36ea243da17dae2020d95ea
Linda Wong <Linda Wong>
2016-11-21 22:58:01 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+// told to comment, not given anything that needs a comment.  so, COMMENT.
